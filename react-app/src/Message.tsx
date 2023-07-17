e7c0e55f0b1699e53e064196b29020d46c6c4652
Renan Rodrigues <renan_ime_2012@hotmail.com>
2023-07-17 17:19:54 -0300

    continue

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,13 @@
+
+
+function Message(){
+    const name = '';
+    
+    if (name)
+      return<h1>Hello {name}</h1>
+    return <h1>Hello World</h1>
+    //Esse código debaixo do capô é compilado para javascript
+    //babeljs.io/repl
+}
+
+export default Message;
