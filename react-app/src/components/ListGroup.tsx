d53935aefc4f96745d454f893bde327f48d096a7
Renan Rodrigues <renan_ime_2012@hotmail.com>
2023-07-18 14:36:39 -0300

    handle event

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,13 +1,24 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
+    let items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ]
+
+    const handleClick = (event:MouseEvent) => console.log(event);
+
+
     return (
         <>
             <h1>List</h1>
+            {items.length === 0 && <p>No item found</p>}{/*  items.length === 0 ? <p>No item found</p> : null */}
             <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
+                {items.map((item,index) => <li key={item} 
+                                       className="list-group-item" onClick={handleClick}>{item}</li>)}
             </ul>
         </>
     );
